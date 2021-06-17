bb8a95b1e7780d0f4bb403064fd587e66499e45c
DavDan1 <david-danielyan@hotmail.com>
2021-06-17 16:17:41 +0200

    adds links to headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import NavigationButtons from './NavigationButtons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
+      <Link>
+      
+      
+      </Link>
       <div data-cy='header-navigation-container'>
         <h1 data-cy='header-title'>Course List</h1>
         <NavigationButtons  />
