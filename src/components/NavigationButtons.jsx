74ead7f62888b949b760af3f02217d1642e67047
DavDan1 <david-danielyan@hotmail.com>
2021-06-17 12:13:16 +0200

    adds header and buttons components for navbar

---
+++ src/components/NavigationButtons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavigationButtons = () => {
+  return (
+    <div>
+      <button>Workshops</button>
+      <button>Testing</button>
+      <button>Vue</button>
+      <button>Productivity</button>
+      <button>Node</button>
+      <button>React</button>
+    </div>
+  );
+};
+
+export default NavigationButtons;
